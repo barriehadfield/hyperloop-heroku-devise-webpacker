e56f441268d834814eb8a177c8b1677b673d19ac
Barrie Hadfield <barrie@workshare.com>
2017-07-28 12:35:17 +0100

    no more automatic config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require 'react_ujs'
+//= require 'jquery'
+//= require 'jquery_ujs'
 //= require_tree .
-//= require jquery
-//= require jquery_ujs
-//= require hyperloop-loader
+
+//= require 'components'
+Opal.load('components');
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,12 @@ module Monkee
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.hyperloop.auto_config = false
+
+    config.eager_load_paths -= %W[#{config.root}/app/hyperloop]
+    config.eager_load_paths += %W[#{config.root}/app/hyperloop/models]
+    config.eager_load_paths += %W[#{config.root}/app/hyperloop/operations]
+
   end
 end
